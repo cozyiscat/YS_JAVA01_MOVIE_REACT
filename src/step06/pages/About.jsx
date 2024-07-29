5174442b321cedfb11834dce30d7931b246ca0bb
cozyiscat <yuiop7664@gmail.com>
2024-07-29 14:39:12 +0900

    step06 About style / build

---
+++ src/step06/pages/About.jsx
@@ -0,0 +1,9 @@
+import './About.css';
+export function About(){
+    return (
+        <div className='about_container'>
+            <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum perspiciatis quo asperiores, assumenda iusto soluta delectus laudantium aliquid quae omnis itaque modi tempora officia aut quasi eveniet! Harum, dolore nisi.</span>
+            <span>영화리뷰사이트, 2024</span>
+        </div>
+    )
+}
---
+++ src/step06/pages/Detail.jsx
@@ -0,0 +1,9 @@
+import './Detail.css';
+export function Detail(){
+    return (
+        <div className='detail'>
+            <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum perspiciatis quo asperiores, assumenda iusto soluta delectus laudantium aliquid quae omnis itaque modi tempora officia aut quasi eveniet! Harum, dolore nisi.</span>
+            <span>영화리뷰사이트, 2024</span>
+        </div>
+    )
+}
