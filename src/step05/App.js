144ab078e0b5b8936e528367a2dc7411309898c1
cozyiscat <yuiop7664@gmail.com>
2024-07-29 14:28:36 +0900

    step05 네비게이션 연결

--- src/step05/App.js
+++ src/step05/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import {Detail} from './pages/Detail';
+import { Navigation } from './components/Navigation';
 function App() {
   return (
     <BrowserRouter>
+    <Navigation/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
